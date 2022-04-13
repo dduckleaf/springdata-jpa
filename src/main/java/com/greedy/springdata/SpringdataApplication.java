f13c2e7e7852ec6f364e95825cb23cee0adfc876
Minjoo Jang <miinjoojang@gmail.com>
2022-04-14 02:52:14 +0900

    [INIT] 프로젝트 생성

--- src/main/java/com/greedy/springdata/SpringdataApplication.java
+++ src/main/java/com/greedy/springdata/SpringdataApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringdataApplication {
-
+    /* TEST */
     public static void main(String[] args) {
         SpringApplication.run(SpringdataApplication.class, args);
     }
