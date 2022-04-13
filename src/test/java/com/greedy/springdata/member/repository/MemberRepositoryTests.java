a79094e02cb2291900dc44c55392f1438f38cc49
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-04-14 04:08:46 +0900

    no message

--- src/test/java/com/greedy/springdata/member/repository/MemberRepositoryTests.java
+++ src/test/java/com/greedy/springdata/member/repository/MemberRepositoryTests.java
@@ -1,4 +1,44 @@
+package com.greedy.springdata.member.repository;
+
+import com.greedy.springdata.config.JpaConfiguration;
+import com.greedy.springdata.config.SpringdataApplication;
+import com.greedy.springdata.member.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ContextConfiguration(classes = {
+        SpringdataApplication.class,
+        JpaConfiguration.class,
+        com.greedy.springdata.config.ContextConfiguration.class
+})
 class MemberRepositoryTests {
-  
+
+    @Autowired
+    private MemberRepository repo;
+
+    @Test
+    public void initTest() {
+
+        assertNotNull(repo);
+    }
+    
+    @Test
+    @DisplayName("회원조회 테스트")
+    public void findMemberById_test() {
+        String username = "admin";
+        
+        Member member = repo.findMemberByMemberId(username);
+        
+        assertNotNull(member);
+        System.out.println("member = " + member);
+        
+    }
+
+
 }
