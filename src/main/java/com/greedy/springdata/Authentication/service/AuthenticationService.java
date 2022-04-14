b90c77dac502f699506d427a156aa99b79faff98
DESKTOP-ONN8F4O\h99ww <h99www@gmail.com>
2022-04-15 02:50:54 +0900

    FEAT: test2

--- src/main/java/com/greedy/springdata/Authentication/service/AuthenticationService.java
+++ src/main/java/com/greedy/springdata/Authentication/service/AuthenticationService.java
@@ -3,5 +3,6 @@ package com.greedy.springdata.Authentication.service;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 //test1
+//test2
 public interface AuthenticationService extends UserDetailsService {
 }
